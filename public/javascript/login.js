1e8d9267de0968bc1c1f7963b1341df6054d9c0f
Derimar Gray <derimargray@gmail.com>
2021-09-22 18:40:43 -0500

    signin working

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,7 +2,7 @@ async function loginFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector('#username-input-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const password = document.querySelector('#password-input-login').value.trim();
 
   if (username && password) {
     const response = await fetch('/api/users/login', {
