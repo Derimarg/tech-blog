b45d7741801d8720cf9a7b620c973d2aec8c2bef
Derimar Gray <derimargray@gmail.com>
2021-09-26 05:25:24 -0500

    existing post view

---
+++ seeds/user-seeds.js
@@ -0,0 +1,48 @@
+const { User } = require('../models');
+
+const userSeeds = [
+    {
+        username: 'aki19',
+        email: 'aki@fakemail.com',
+        password: 'password'
+    },
+    {
+        username: 'derimar95',
+        email: 'derimar@fakemail.com',
+        password: 'password'
+    },
+    {
+        username: 'jose33',
+        email: 'rjose@fakemail.com',
+        password: 'password'
+    },
+    {
+        username: 'john10',
+        email: 'john@fakemail.com',
+        password: 'password'
+    },
+    {
+        username: 'charlhector',
+        email: 'charlh@fakemail.com',
+        password: 'password'
+    },
+    {
+        username: 'kim_lopez',
+        email: 'kim@fakemail.com',
+        password: 'password'
+    },
+    {
+        username: 'shann',
+        email: 'shann@fakemail.com',
+        password: 'password'
+    },
+    {
+        username: 'dg45',
+        email: 'dg45@email.com',
+        password: 'password'
+    }
+];
+
+const users = () => User.bulkCreate(userSeeds, {individualHooks: true});
+
+module.exports = users;
